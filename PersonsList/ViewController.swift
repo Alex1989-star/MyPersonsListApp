83bb35d363ca3871ac2f461e75c95f3446e9dc1f
Alex1989-star <bio181@yandex.ru>
2021-06-09 11:29:39 +0300

    Initial Commit

---
+++ PersonsList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonsList
+//
+//  Created by Леся on 09.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
